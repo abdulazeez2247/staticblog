c4da285c4a09ffaff96c5af98fe86a6aa0495616
abdulazeez2247 <abdulazeezoladokun65@gmail.com>
2025-05-26 10:42:22 +0100

    df

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -12,7 +12,7 @@ export default function Header() {
   return (
     <div className="flex w-[100%] justify-between text-white bg-gray-900 px-10 lg:h-[10vh] items-center">
         <h2 className="text-2xl font-bold">Blog</h2>
-        <ul className="flex gap-3">
+        <ul className="lg:flex hidden gap-3">
           <li className="border py-2 px-6 rounded-sm hover:bg-gradient-to-bl from-gray-950 to-gray-400">
             <Link to="/">Home</Link>
           </li>
