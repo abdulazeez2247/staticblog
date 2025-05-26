64fb6ed7ae584b69b01b492e41dd07523461e58a
abdulazeez2247 <abdulazeezoladokun65@gmail.com>
2025-05-26 10:38:03 +0100

    df

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import Router from "./router/Router";
 import { Bounce, ToastContainer, toast } from "react-toastify";
 
+import 'primeicons/primeicons.css';
+        
 export default function App() {
   return (
     <div>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
+
 export default function Header() {
+  
+    const [shownavbar, setShownavbar] = useState(false);
+    const navtoggle = ()=>{
+      setShownavbar(!notshow)
+    }
+  
   return (
-    <div className="flex justify-between text-white bg-gray-900 px-10 lg:h-[10vh] items-center">
-      <h2 className="text-2xl font-bold">Blog</h2>
-      <ul className="flex gap-3">
-        <li className="border py-2 px-6 rounded-sm hover:bg-gradient-to-bl from-gray-950 to-gray-400">
-          <Link to="/">Home</Link>
-        </li>
-        <li className="border py-2 px-6 rounded-sm hover:bg-gradient-to-bl from-gray-950 to-gray-400">
-          <Link to="/Aboutuspage">About</Link>
-        </li>
-        <li className="border py-2 px-6 rounded-sm hover:bg-gradient-to-bl from-gray-950 to-gray-400">
-          <Link to="/addblog">Add-blog</Link>
-        </li>
-      </ul>
+    <div className="flex w-[100%] justify-between text-white bg-gray-900 px-10 lg:h-[10vh] items-center">
+        <h2 className="text-2xl font-bold">Blog</h2>
+        <ul className="flex gap-3">
+          <li className="border py-2 px-6 rounded-sm hover:bg-gradient-to-bl from-gray-950 to-gray-400">
+            <Link to="/">Home</Link>
+          </li>
+          <li className="border py-2 px-6 rounded-sm hover:bg-gradient-to-bl from-gray-950 to-gray-400">
+            <Link to="/Aboutuspage">About</Link>
+          </li>
+          <li className="border py-2 px-6 rounded-sm hover:bg-gradient-to-bl from-gray-950 to-gray-400">
+            <Link to="/addblog">Add-blog</Link>
+          </li>
+        </ul>
+        <div className='lg:hidden'>
+          <i className=' pi pi-align-justify'></i>
+        </div>
     </div>
+    
+   
   );
 }
