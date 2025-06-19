20fa89256e9df594f16d44fb93b9a6ccebcb9cdf
abdulazeez2247 <abdulazeezoladokun65@gmail.com>
2025-06-19 13:13:26 +0100

    blog2

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,5 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import Homepage from '../pages/Homepage';
-import Aboutuspage from '../pages/Aboutuspage';
-import Addblog from '../pages/Addblog';
-import { loginUser, registeruser } from '../data/newsdata';
 
 
 export default function Header() {
@@ -17,19 +13,19 @@ export default function Header() {
     <div className="flex w-[100%] justify-between text-white bg-gray-900 px-10 h-[10vh] items-center">
         <h2 className="text-2xl font-bold">Blog</h2>
         <ul className="lg:flex hidden gap-3">
-          <li onClick={Homepage} className="border py-2 px-6 bg-blue-500 rounded-tl-sm rounded-tr-sm rounded-bl-sm rounded-br-sm hover:bg-gradient-to-tr from-blue-400 to-blue-900">
+          <li className="border py-2 px-6 bg-blue-500 rounded-tl-sm rounded-tr-sm rounded-bl-sm rounded-br-sm hover:bg-gradient-to-tr from-blue-400 to-blue-900">
             <Link to="/">Home</Link>
           </li>
-          <li onClick={Aboutuspage} className="border py-2 px-6 rounded-sm hover:bg-gradient-to-bl from-gray-950 to-gray-400">
+          <li className="border py-2 px-6 rounded-sm hover:bg-gradient-to-bl from-gray-950 to-gray-400">
             <Link to="/Aboutuspage">About</Link>
           </li>
-          <li onClick={Addblog} className="border py-2 px-6 rounded-sm hover:bg-gradient-to-bl from-gray-950 to-gray-400">
+          <li className="border py-2 px-6 rounded-sm hover:bg-gradient-to-bl from-gray-950 to-gray-400">
             <Link to="/addblog">Add-blog</Link>
           </li>
-          <li onClick={registeruser} className="border py-2 px-6 rounded-sm hover:bg-gradient-to-bl from-gray-950 to-gray-400">
+          <li className="border py-2 px-6 rounded-sm hover:bg-gradient-to-bl from-gray-950 to-gray-400">
             <Link to="/register">Register</Link>
           </li>
-          <li onClick={loginUser} className="border py-2 px-6 rounded-sm hover:bg-gradient-to-bl from-gray-950 to-gray-400">
+          <li className="border py-2 px-6 rounded-sm hover:bg-gradient-to-bl from-gray-950 to-gray-400">
             <Link to="/login">Login</Link>
           </li>
         </ul>
@@ -55,10 +51,10 @@ export default function Header() {
               <Link to="/addblog">Add-blog</Link>
             </li>
             <li className="border py-2 px-6 rounded-sm hover:bg-gradient-to-bl from-gray-950 to-gray-400">
-            <Link to="/registerpage">Register</Link>
+            <Link to="/register">Register</Link>
           </li>
           <li className="border py-2 px-6 rounded-sm hover:bg-gradient-to-bl from-gray-950 to-gray-400">
-            <Link to="/loginpage">Login</Link>
+            <Link to="/login">Login</Link>
           </li>
           </ul>
           )
